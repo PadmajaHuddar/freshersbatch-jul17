4b4e241286fbe2d5334c40c09c5991273f91470d
Padmaja Huddar <padmaja.huddar@xoriant.com>
2017-07-06 08:28:05 +0530

    js-assignments second commit

--- js1.js
+++ js1.js
@@ -3,10 +3,10 @@
 <body>
 
 <h2>My First JavaScript</h2>
-
+<h2>Assignment</h2>
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+Click me to display the Date and Time.</button>
 
 <p id="demo"></p>
 
